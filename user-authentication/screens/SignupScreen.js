8f9c196025c0e94f1e698df39d106c32c7289891
sailakshmy <sailakshmy94@gmail.com>
2025-04-13 01:20:41 +0400

    Linked the createUser function in the Signup screen

--- user-authentication/screens/SignupScreen.js
+++ user-authentication/screens/SignupScreen.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "../utils/auth";
 
 function LoginScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  async function signup({ email, password }) {
+    try {
+      setIsAuthenticating(true);
+      await createUser(email, password);
+      setIsAuthenticating(false);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user. Please check your input."
+      );
+      setIsAuthenticating(false);
+      return;
+    }
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={signup} />;
 }
 
 export default LoginScreen;
--- user-authentication/utils/auth.js
+++ user-authentication/utils/auth.js
@@ -1,10 +1,12 @@
 const API_KEY = "AIzaSyBKrobPpY9x1n6gV6xmDB9Xg4rMBdmJzGE";
 const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
+  console.log("object", email, password);
   const res = await axios.post(SIGNUP_URL, {
-    email,
-    password,
+    email: email,
+    password: password,
     returnSecureToken: true,
   });
+  console.log("res", res);
 }
