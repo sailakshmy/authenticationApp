9bb707f656a61036ab3956064d2c3c03f469f1e8
sailakshmy <sailakshmy94@gmail.com>
2025-04-14 22:58:44 +0400

    Linked the loginUser function in the LoginScreen

--- user-authentication/screens/LoginScreen.js
+++ user-authentication/screens/LoginScreen.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { loginUser } from "../utils/auth";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  async function loginHandler({ email, password }) {
+    try {
+      setIsAuthenticating(true);
+      await loginUser(email, password);
+      setIsAuthenticating(false);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not authenticate user. Please check your input."
+      );
+      setIsAuthenticating(false);
+      return;
+    }
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- user-authentication/utils/auth.js
+++ user-authentication/utils/auth.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 const API_KEY = "AIzaSyBKrobPpY9x1n6gV6xmDB9Xg4rMBdmJzGE";
-const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 
-export async function createUser(email, password) {
-  console.log("object", email, password);
-  const res = await axios.post(SIGNUP_URL, {
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const res = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true,
   });
-  console.log("res", res);
+  console.log(res.data, "res.data");
+}
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
+}
+
+export async function loginUser(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
