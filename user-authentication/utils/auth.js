b600bf3110b11095534071bd0e97da72cea28ed1
sailakshmy <sailakshmy94@gmail.com>
2025-04-14 22:49:55 +0400

    Fixed the issue with the sign up function

--- user-authentication/utils/auth.js
+++ user-authentication/utils/auth.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const API_KEY = "AIzaSyBKrobPpY9x1n6gV6xmDB9Xg4rMBdmJzGE";
 const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 
