3ad55e15e627e761f175756629de473dbcc55446
sailakshmy <sailakshmy94@gmail.com>
2025-04-17 23:19:42 +0400

    Used the token to access protected resources

--- user-authentication/screens/WelcomeScreen.js
+++ user-authentication/screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authenticationContext = useContext(AuthContext);
+  const token = authenticationContext.token;
+  useEffect(() => {
+    axios
+      .get(
+        `https://react-native-app-7adb7-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching message:", error.message);
+      });
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
